d29a07c8dd09bda47784664ccb4a667fe6a4f17f
Ramonywangziyao <tonywangziyao@gmail.com>
2017-03-29 12:44:29 -0400

    update

--- Suitup/Pods/Target Support Files/Pods-Suitup/Pods-Suitup-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Suitup : NSObject
-@end
-@implementation PodsDummy_Pods_Suitup
-@end
--- Suitup/Pods/Target Support Files/Pods-SuitupUITests/Pods-SuitupUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SuitupUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SuitupUITests
-@end
